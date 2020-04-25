96e3405aa3e7c0f1d11cbd6a9436916291fbc517
amazingproducer <amazingproducer@gmail.com>
2020-04-25 14:27:35 +0000

    fiddle with message sending in new syntax

--- dispatcher.py
+++ dispatcher.py
@@ -4,6 +4,7 @@ import discord
 import os
 
 bot_id = os.environ['ES_DISPATCHER_DISCORD_API_ID']
+dev_id = int(os.environ['ES_DEVELOPER_DISCORD_USER_ID'])
 
 class DispatcherDiscord(discord.Client):
     async def on_connect(self):
@@ -12,5 +13,18 @@ class DispatcherDiscord(discord.Client):
     async def on_ready(self):
         print(f'Dispatcher {self.user} ready.')
 
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        await message.channel.send("DidahDiddit")
+        print(f"Fetching dev user object: {dev_id}")
+        dev_dm_id = await self.fetch_user(int(dev_id))
+        print(f"Found object for dev user {dev_dm_id.name}.")
+        await dev_dm_id.send("Didahdahdit")
+
+
+# In the absence of an API to receive dispatch requests, demonstrate DM notifications when a user post receives a reaction.
+#    async def on_reaction_add(reaction, user):
+print(dev_id, bot_id)
 dispatcher = DispatcherDiscord()
 dispatcher.run(bot_id)
